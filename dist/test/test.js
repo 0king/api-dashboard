7f6540b212795ddf7fe0d1794d0ded6766e727a2
durjan <durgakne@gmail.com>
2024-12-08 19:56:27 +0530

    added prisma schema

---
+++ dist/test/test.js
@@ -0,0 +1,16 @@
+"use strict";
+//import { createRequire } from "module";
+//const require = createRequire(import.meta.url);
+//or
+/*
+import readline from 'readline';
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("What's up?\n", data => {
+    console.log(`Got your result: ${data}`);
+    rl.close();
+});
+*/
---
+++ src/test/test.ts
@@ -0,0 +1,15 @@
+//import { createRequire } from "module";
+//const require = createRequire(import.meta.url);
+//or
+/* 
+import readline from 'readline';
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("What's up?\n", data => {
+    console.log(`Got your result: ${data}`);
+    rl.close();
+}); 
+*/
